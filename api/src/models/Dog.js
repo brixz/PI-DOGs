2ed0a2374003cf3a9476b09b4073456c54a32c21
brixz <diazb6904@gmail.com>
2023-01-15 17:29:06 -0300

    dt y relaciones

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
-    name: {
+    id:{
+      type: DataTypes.UUIDV1,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey:true
+    },
+    name:{
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height:{
+      type: DataTypes.INTEGER,
+    },
+    weight:{
+      type: DataTypes.STRING
+    },
+    life_span:{
+      type: DataTypes.NUMBER,
+    },
+    image:{
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdInDataBase: {
+      type: DataTypes.BOOLEAN, 
+      allowNull: false,
+      defaultValue: true,
+      }
   });
 };
