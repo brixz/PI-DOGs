cea9d66a0b905b2874c7b9f99e90a510f126269d
brixz <diazb6904@gmail.com>
2023-01-19 22:46:45 -0300

    jnj

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogs = require('./dogs');
+const temp = require('./temperaments')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/dogs', dogs);
+router.use('/temperaments', temp);
 
 module.exports = router;
