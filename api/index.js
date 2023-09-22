d9f73bd092db417f1575d62ad3f8456ba34b7799
brixz <diazb6904@gmail.com>
2023-09-21 23:45:12 -0300

    ignore

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const port = process.env.PORT || 7367;
+const port = process.env.PORT;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
